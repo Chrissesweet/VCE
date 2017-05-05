2f3ff3684c26ca31b8653cc40f8178dd57ece85c
Chrissesweet <chrisse.e@hotmail.com>
2017-05-05 08:30:01 +0200

    Map with location pin on it

--- V-TAG/V-TAG/ViewController.swift
+++ V-TAG/V-TAG/ViewController.swift
@@ -1,25 +1,53 @@
 //
 //  ViewController.swift
-//  V-TAG
+//  vce2017
 //
-//  Created by Christian Elawad on 2017-05-05.
+//  Created by Christian Elawad on 2017-05-04.
 //  Copyright © 2017 Christian Elawad. All rights reserved.
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Map: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        var location = CLLocationCoordinate2DMake(56.182850383, 15.591162818)
+        
+        
+        var span = MKCoordinateSpanMake(0.002, 0.002)
+        
+        var region = MKCoordinateRegion(center: location, span: span)
+        
+        Map.setRegion(region, animated: true)
+        
+        var annotation = MKPointAnnotation()
+        
+        annotation.coordinate = location
+        annotation.title = "stone"
+        annotation.subtitle = "hassans"
+        
+        Map.addAnnotation(annotation)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
+
 
