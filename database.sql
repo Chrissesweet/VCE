de7e3894bc1551c95a4d0c7b6c360a5647386ed4
Chrissesweet <chrisse.e@hotmail.com>
2017-05-05 08:20:27 +0200

    iOS - mysql connection initial

--- database.sql
+++ database.sql
@@ -1,9 +1,12 @@
+
+DROP DATABASE IF EXISTS VolvoCE;
+
 CREATE DATABASE VolvoCE;
 
-DROP TABLE IF EXISTS 'tag';
+DROP TABLE IF EXISTS tag;
 
 CREATE TABLE tag(
-tagId int AUTO_INCREMENT,
+tagId int NOT NULL AUTO_INCREMENT,
 orderId varchar(250),
 longitude varchar(250),
 latitude varchar(250),
@@ -13,24 +16,27 @@ PRIMARY KEY(tagId)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-DROP TABLE IF EXISTS 'order';
+DROP TABLE IF EXISTS `order`;
 
-CREATE TABLE order (
-orderId int AUTO_INCREMENT,
+CREATE TABLE `order` (
+`orderId` int NOT NULL AUTO_INCREMENT,
 materialType varchar(250),
 owner varchar(250),
 PRIMARY KEY(orderId)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-DROP TABLE IF EXISTS booking;
+DROP TABLE IF EXISTS `booking`;
 
 CREATE TABLE booking (
-bookingId int AUTO_INCREMENT,
+bookingId int NOT NULL AUTO_INCREMENT,
 longitude varchar(250),
 latitude varchar(250),
 startDateTime datetime,
 endDateTime datetime,
-PRIMARY KEY(cid)
+PRIMARY KEY(bookingId)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+INSERT INTO `tag` (`tagId`, `orderId`, `longitude`, `latitude`, `attitude`, `ammountLeft`) VALUES ('1', 'Stone 2tons 981232', '15.591162818', '56.182850383', '-', '1tons');
+INSERT INTO `tag` (`tagId`, `orderId`, `longitude`, `latitude`, `attitude`, `ammountLeft`) VALUES ('2', 'Pipes 20 981232', '15.591162830', '56.182850390', '-', '10');
---
+++ swift 3 - mysql/Config.php
@@ -0,0 +1,8 @@
+<?php
+        define('DB_USERNAME', 'root');
+        define('DB_PASSWORD', '');
+        define('DB_HOST', '172.20.10.5');
+        define('DB_NAME', 'VolvoCE');
+?>
+
+
